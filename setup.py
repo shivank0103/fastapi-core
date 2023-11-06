7954cc91db4946ea5861c0ddca005b1693894e16
shivank0103 <shivank0103@gmail.com>
2023-11-06 16:25:37 +0530

    fix: revertin setup name

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name='cashifycore',
+    name='fastapi_core',
     version='0.0.1',
     description='Core for Fast API projects',
     url='https://github.com/shivank0103/fastapi-core-project',
